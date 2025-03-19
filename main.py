53e2dd9ce3d9440eec73f74ec9dc41a8f33500de
esantanamat <esantanamat@gmail.com>
2025-03-19 13:37:11 -0400

    postgresql python connection testing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, Float, DateTime, CHAR
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
 
 Base = declarative_base()
@@ -23,5 +25,23 @@ class Student(Base):
     def __repr__(self):
         return f"({self.id} {self.firstname} {self.lastname}, {self.grade})"
     
-    engine = create_engine('postgresql://username:password@localhost:5432/dbname')
-    
+
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "5432") 
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+person = Student(12315, 'John', 'Wall', 12 )
+person2 = Student(1111, 'Joe', 'Burrow', 10)
+session.add(person)
+session.add(person2)
+session.commit()
